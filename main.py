4b4adecf61d2dfc0f548ab5f6563d05a3a6e1412
k-seki <seki@magnetic.co.jp>
2020-05-08 18:44:27 +0900

    use leaflet

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import json
 
 
 @dataclass()
-class Place(object):
-    name: str
+class LatLng(object):
     lat: float
     lng: float
 
@@ -17,23 +16,23 @@ class Main(responder.API):
 
         self.add_route('/', self.top)
 
-        self.place: List[Place] = [
-            Place(name='函館支店', lat=41.766021, lng=140.718940),
-            Place(name='釧路営業所', lat=42.982795, lng=144.383783),
-            Place(name='稚内営業所', lat=45.409663, lng=141.674238),
-            Place(name='仙台支店', lat=38.261555, lng=140.886887),
-            Place(name='八戸営業所', lat=40.527124, lng=141.536147),
-            Place(name='辰巳事業所', lat=35.652416, lng=139.809551),
-            Place(name='焼津営業所', lat=34.863612, lng=138.321426),
-            Place(name='関西支店', lat=34.698559, lng=135.490293),
-            Place(name='高知営業所', lat=31.552530, lng=133.563696),
-            Place(name='九州支店', lat=33.596823, lng=130.407840),
-            Place(name='長崎営業所', lat=32.752861, lng=129.865224),
-            Place(name='鹿児島営業所', lat=31.558395, lng=130.557021),
-        ]
+        self.place: Dict[str, LatLng] = {
+            '函館支店': asdict(LatLng(lat=41.766021, lng=140.718940)),
+            '釧路営業所': asdict(LatLng(lat=42.982795, lng=144.383783)),
+            '稚内営業所': asdict(LatLng(lat=45.409663, lng=141.674238)),
+            '仙台支店': asdict(LatLng(lat=38.261555, lng=140.886887)),
+            '八戸営業所': asdict(LatLng(lat=40.527124, lng=141.536147)),
+            '辰巳事業所': asdict(LatLng(lat=35.652416, lng=139.809551)),
+            '焼津営業所': asdict(LatLng(lat=34.863612, lng=138.321426)),
+            '関西支店': asdict(LatLng(lat=34.698559, lng=135.490293)),
+            '高知営業所': asdict(LatLng(lat=31.552530, lng=133.563696)),
+            '九州支店': asdict(LatLng(lat=33.596823, lng=130.407840)),
+            '長崎営業所': asdict(LatLng(lat=32.752861, lng=129.865224)),
+            '鹿児島営業所': asdict(LatLng(lat=31.558395, lng=130.557021)),
+        }
 
     def top(self, req: responder.Request, resp: responder.Response):
-        resp.content = self.template(filename='main.html', place=self.place)
+        resp.content = self.template(filename='main.html', place=json.dumps(self.place))
 
 
 api = Main()
